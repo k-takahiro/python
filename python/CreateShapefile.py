d2a27ffeecf544261a668aabdc8fee3701a6b149
r-nakajun <nakajunLearn@outlook.jp>
2024-12-17 08:20:12 +0900

    springappからpythonを避難

---
+++ python/CreateShapefile.py
@@ -0,0 +1,22 @@
+import shapefile
+
+# Shapefileの読み込み
+shp_path = 'demo\\work\\python\\P04-14_47_GML\\P04-14_47-g_MedicalInstitution.shp'
+sf = shapefile.Reader(shp_path, encoding='cp932')
+
+print("Geometry:")
+for shape in sf.shapes():
+    print(shape, '/', shape.shapeType, '/', shape.points)
+
+# 属性データのフィールド名の表示
+print("\nFields:")
+fields = sf.fields[1:]  # 最初のフィールドは削除フラグであるためスキップ
+field_names = [field[0] for field in fields]
+print(field_names)
+
+# 属性データの表示
+print("\nRecords:")
+for record in sf.records():
+    print(record)
+
+# py -m pip install pyshp
---
+++ sqlite/sqlite_practice.sql
@@ -0,0 +1,2 @@
+-- SQLite
+INSERT INTO user_tb (id, name) VALUES (1, "A1");
